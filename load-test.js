8283fe40ac9f6e2f145a71efaa5c72789af2d568
Alias1177 <mamedovalik267@gmail.com>
2025-02-15 20:21:37 +0400

    Update load-test.js

--- load-test.js
+++ load-test.js
@@ -5,24 +5,23 @@ export let options = {
     scenarios: {
         constant_request_rate: {
             executor: 'constant-arrival-rate',
-            rate: 1000,              // 1000 итераций (запросов) в секунду
-            timeUnit: '1s',          // за одну секунду
-            duration: '10s',          // длительность теста – 1 минута
-            preAllocatedVUs: 1000,    // заранее выделяем 100 виртуальных пользователей
-            maxVUs: 1000,            // максимум может быть до 1000 VUs при пиковых нагрузках
+            rate: 1000,            
+            timeUnit: '1s',          
+            duration: '10s',          
+            preAllocatedVUs: 1000,    
+            maxVUs: 1000,           
         },
     },
     thresholds: {
-        // 95% запросов должны выполняться менее чем за 50 мс
+       
         http_req_duration: ['p(95)<50'],
-        // Доля неуспешных запросов (ошибок) должна быть меньше 0.01% (то есть успешность ≥ 99.99%)
+     
         http_req_failed: ['rate<0.0001'],
     },
 };
 
 export default function () {
-    // Здесь укажите URL вашего тестируемого эндпоинта.
-    // Если эндпоинт защищён JWT, не забудьте заменить <your_jwt_token> на валидный токен.
+//Вставте свой токен после регистрации
     let res = http.get('http://localhost:8080/api/info', {
         headers: {
             'Authorization': 'Bearer  <your_jwt_token>',
@@ -33,6 +32,6 @@ export default function () {
         'status is 200': (r) => r.status === 200,
     });
 
-    // Минимальная задержка, чтобы итерация не зацикливалась мгновенно.
+  
     sleep(0.001);
 }
